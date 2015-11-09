970a920acf27b88258ef4244edfa53efe396d536
Tanaka Kazunari <tzskp1@gmail.com>
2015-11-09 15:00:54 +0900

    check whether emacs 24.4 is installed

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
+function install_emacs(){
+	wget -O-  http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.xz | tar xJvf -
+	cd emacs-24.4
+	./configure && make install
+}
 
 nh=$(pwd)
-apt-get install zsh
+yes | sudo apt-get install zsh build-essential
+yes | sudo apt-get build-dep emacs24
 
 # emacs 24.4
-wget -O-  http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.xz | tar xJvf -
-cd emacs-24.4
-./configure && make install
+which emacs > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	str=$(emacs --version | grep -e "24.4" | awk '{print 1}')
+	if [ ! $str ]; then
+		install_emacs
+	fi
+else
+	install_emacs
+fi
 
 # login shell
 chsh -s /usr/bin/zsh
