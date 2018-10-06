2dc6287f8349dd3aa2938a40cf579bfbf1689316
Tanaka Kazunari <tzskp1@gmail.com>
2018-10-06 14:14:22 +0900

    fix makefile.

--- install.mk
+++ install.mk
@@ -11,32 +11,34 @@ define make_symlink
 	@ln -sfn $(MAKEFILE_DIR)$@ ~/$@
 endef
 
-zsh:
+define zsh
 	@cat << EOF > ~/.zshenv
-	#! /usr/bin/env zsh
-	# -*- mode: sh ; coding: utf-8 -*- 
-	#--- copy to each machine ---
-	export ZDOTDIR=$(MAKEFILE_DIR)/zsh
-	source ${ZDOTDIR}/.zshenv
-	#---------------------------- " 
-	EOF
+#! /usr/bin/env zsh
+# -*- mode: sh ; coding: utf-8 -*- 
+#--- copy to each machine ---
+export ZDOTDIR=$(MAKEFILE_DIR)/zsh
+source ${ZDOTDIR}/.zshenv
+#---------------------------- " 
+EOF
 	@cd ~
 	@curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+endef
 
-lesskey:
+define lesskey
 	@lesskey <<EOF
-	#command
-	m               repeat-search 
-	\em             repeat-search-all 
-	M               reverse-search 
-	\eM             reverse-search-all 
-	h               forw-line 
-	t               back-line 
-	H               forw-line-force 
-	T               back-line-force 
-	EOF
+#command
+m               repeat-search 
+\em             repeat-search-all 
+M               reverse-search 
+\eM             reverse-search-all 
+h               forw-line 
+t               back-line 
+H               forw-line-force 
+T               back-line-force 
+EOF
+endef
 
-misc:
+define misc
 	@cd ~
 
 	@go get github.com/peco/peco/cmd/peco
@@ -54,6 +56,16 @@ misc:
 	@git clone https://github.com/gmmeyer/awesome-dangerzone
 	@mv /tmp/awesome-dangerzone/icons ~/.icons
 	@rm -rf /tmp/awesome-dangerzone
+endef
+
+zsh:
+	$(zsh)
+
+lesskey:
+	$(lesskey)
+
+misc:
+	$(misc)
 
 $(dots):
 	$(make_symlink)
