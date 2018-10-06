f4f008247f769d2b51444f226a79458d70c4779c
Tanaka Kazunari <tzskp1@gmail.com>
2018-10-06 14:25:37 +0900

    change makefile.

--- install.mk
+++ install.mk
@@ -1,9 +1,10 @@
 MAKEFILE_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 dots := .emacs .config .sshrc .sshrc.d .mostrc .dircolors texmf .xmonad
+inits := zsh lesskey peco font icon
 
-.PHONY: all sym $(dots) zsh lesskey misc
+.PHONY: all sym $(dots) $(inits)
 
-all: $(dots) zsh lesskey misc
+all: $(dots) $(inits)
 
 define make_symlink
 	@echo -e "\e[34mCreating symlink of "$@" ..."
@@ -11,50 +12,12 @@ define make_symlink
 	@ln -sfn $(MAKEFILE_DIR)$@ ~/$@
 endef
 
-define lesskey
-	@lesskey <<EOF
-#command
-m               repeat-search 
-\em             repeat-search-all 
-M               reverse-search 
-\eM             reverse-search-all 
-h               forw-line 
-t               back-line 
-H               forw-line-force 
-T               back-line-force 
-EOF
-endef
-
-define misc
-	@cd ~
-
-	@go get github.com/peco/peco/cmd/peco
-
-	@wget https://github.com/adobe-fonts/source-han-code-jp/archive/2.011R.zip
-	@unzip 2.011R.zip
-	@cp ~/source-han-code-jp-2.011R/OTC/SourceHanCodeJP.ttc ~/.local/share/fonts
-
-	@git clone https://github.com/powerline/fonts
-	@cd ~/fonts/
-	@./install.sh
-
-	@cd /tmp
-	@rm -rf ~/.icons
-	@git clone https://github.com/gmmeyer/awesome-dangerzone
-	@mv /tmp/awesome-dangerzone/icons ~/.icons
-	@rm -rf /tmp/awesome-dangerzone
-endef
-
-zsh:
-	$(MAKEFILE_DIR)init/zsh
-
-lesskey:
-	$(lesskey)
-
-misc:
-	$(misc)
+$(inits):
+	$(MAKEFILE_DIR)init/$@
 
 $(dots):
 	$(make_symlink)
 
 sym: $(dots)
+
+init: $(inits)
