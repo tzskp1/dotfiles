ab9a401f3ffddb09b96f3e81303ef32a94fc98a8
Kazunari Tanaka <tzskp1@gmail.com>
2024-03-21 15:32:29 +0900

    Fix nixglhypr

--- nixglhypr/default.nix
+++ nixglhypr/default.nix
@@ -4,14 +4,11 @@ stdenv.mkDerivation {
   pname = "nixglhypr";
   version = "0.1.0";
   src = ./nixglhypr;
-  checkInputs = [
-    gnused
-    gnugrep
-    findutils
-    patchelfStable
-  ];
   buildCommand = ''
     mkdir -p $out/bin
     cp $src $out/bin/nixglhypr
+    substituteInPlace $out/bin/nixglhypr \
+      --replace "patchelf" \
+      "${pkgs.patchelf}/bin/patchelf"
   '';
 }
