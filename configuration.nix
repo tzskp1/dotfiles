635070c6cbd75591aa97070c9f9e4ea402b79e64
Kazunari Tanaka <tzskp1@gmail.com>
2025-01-18 09:29:52 +0900

    Change pinentry

--- configuration.nix
+++ configuration.nix
@@ -161,7 +161,7 @@
 
   programs.gnupg.agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-tty;
+    pinentryPackage = pkgs.pinentry-gnome3;
     enableSSHSupport = true;
   };
   services.pcscd.enable = true;
